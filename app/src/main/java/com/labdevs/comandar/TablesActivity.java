cf65988e8419960c365b6de2a88b4e7034dc7387
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-10-25 20:27:00 -0300

    Modificacion de configuracion base de la actividad

--- app/src/main/java/com/labdevs/comandar/TablesActivity.java
+++ app/src/main/java/com/labdevs/comandar/TablesActivity.java
@@ -7,18 +7,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.labdevs.comandar.databinding.ActivityTablesBinding;
+import com.labdevs.comandar.viewmodels.LoginViewModel;
 
 public class TablesActivity extends AppCompatActivity {
+
+    private ActivityTablesBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_tables);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityTablesBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
