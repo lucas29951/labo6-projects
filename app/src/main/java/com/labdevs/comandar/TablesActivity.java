4295a7335abbc11e96efb68c6df2b562a4763252
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-10-26 00:34:41 -0300

    Imports innecesarios eliminados

--- app/src/main/java/com/labdevs/comandar/TablesActivity.java
+++ app/src/main/java/com/labdevs/comandar/TablesActivity.java
@@ -2,15 +2,9 @@ package com.labdevs.comandar;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.labdevs.comandar.databinding.ActivityTablesBinding;
-import com.labdevs.comandar.viewmodels.LoginViewModel;
 
 public class TablesActivity extends AppCompatActivity {
 
